a697b52356133896eeafd3d476970bd8caf48bf5
GorkemPalali <97700425+GorkemPalali@users.noreply.github.com>
2022-12-06 15:08:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// let FName='Görkem'
+// let LName= 'Palalı'
+// let ID='62008353984'
+
+// let order1=100.2
+// let order2=150.5
+
+// let total=parseInt(order1 + order2)
+// let sex='Male'
+// let address='küçükçekmece'
+
+
+// console.log(FName)
+// console.log(LName)
+// console.log(ID)
+// console.log(total)
+// console.log(sex)
+// console.log(address)
+
+// const yearOfBirth=2001
+// let val
+// val = new Date().getFullYear()
+// console.log(val-yearOfBirth)
+
+// let course = 'Modern JavaScript Kursu'
+// console.log(course.length)
+
+
+// let WeightCan=70
+// let HeightCan=1.80
+// let WeightAda=100
+// let HeightAda=1.60
+
+// let i1 = (WeightCan)/(HeightCan*HeightCan)
+// let i2 = (WeightAda)/(HeightAda*HeightAda)
+// console.log(i1)
+// console.log(i2)
+
+// if(i1>0 && i1<18.4)
+//     console.log('Zayıf')
+//     else if(i1>18.5 && i1<24.9)
+//     console.log('Normal')
+//     else if (i1>25.0 && i1<29.9)
+//     console.log('Fazla Kilolu')
+//     else
+//     console.log('Şişman')
+
+// if(i2>0 && i2<18.4)
+//     console.log('Zayıf')
+//     else if(i2>18.5 && i2<24.9)
+//     console.log('Normal')
+//     else if (i2>25.0 && i2<29.9)
+//     console.log('Fazla Kilolu')
+//     else
+//     console.log('Şişman')
+
+
